fb383ca6cc52f4414a186d4c059bdbf8c3fd92af
Owen Mundy <omundy@gmail.com>
2018-07-21 07:10:47 -0400

    Updates tutorial

--- tutorial/content.js
+++ tutorial/content.js
@@ -1,6 +1,1 @@
-{
-    "name": "Explode the Web!",
-    "description": "Explodes web pages that contain data trackers!",
-    "version": "1.0",
-    "manifest_version": 2
-}
+console.log("Hello World!");
