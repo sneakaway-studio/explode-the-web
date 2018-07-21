60c925fe7b0ce9217ef8ed551f6494d5437ff6e1
Owen Mundy <omundy@gmail.com>
2018-07-21 06:52:00 -0400

    Moves tutorial sections in to folder

--- tutorial/sections/3/content.js
+++ tutorial/sections/3/content.js
@@ -21,18 +21,20 @@ for (var i = 0, l = scriptsFound.length; i < l; i++) {
 // if there are trackers on the page
 if (trackersFound.length > 0) {
 	// create html element to show trackers
-	var str = "<div class='explode-notification'>";
-    // display the number of trackers
+	var str = "<div class='explode-notification' title='click to see trackers on this page'>";
+	// display the number of trackers
 	str += "<span class='explode-number'>" + trackersFound.length + "</span>";
 	str += "<span class='explode-text'>" + " tracker(s)" + "</span>";
 	str += "</div>";
-    // create the list of trackers
+	// create the list of trackers (hide by default)
 	str += "<div class='explode-tracker-list'>" + trackersFound.join("<br>") + "</div>";
-	// append html to page
+	// timer (hide by default)
+	str += "<div class='explode-counter'><div class='explode-counter-text'></div></div>";
+	// append html to loaded web page
 	$('body').append(str);
 	// if the user clicks on the element
 	$(document).on('click', '.explode-notification', function() {
-        // and display the list
+		// and display the list
 		$('.explode-tracker-list').toggle();
-	})
+	});
 }
